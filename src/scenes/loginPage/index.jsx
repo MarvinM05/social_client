992a41011388531e29b6ac141b87e3d1d753e6f1
MarvinM05 <merv360@gmail.com>
2023-09-05 21:38:42 -0600

    title app updated

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Perspectivap
+          PerspectivApp
         </Typography>
       </Box>
 
